f578902de87a0260b5aefc5d4ae0921dcec54b20
David Panart <davidpanart@gmail.com>
2017-08-05 10:52:12 +0200

    secure fctn available on client

--- global.js
+++ global.js
@@ -1,22 +1,20 @@
-if ( Meteor.isServer ) {
-	global.secure = collection => {
-		const yes = {
-	    insert	: f => true,
-	    update	: f => true,
-	    remove	: f => true,
-	  };
-	  const no = {
-	    insert	: f => false,
-	    update	: f => false,
-	    remove	: f => false,
-	  };
+global.secure = collection => {
+	const yes = {
+    insert	: f => true,
+    update	: f => true,
+    remove	: f => true,
+  };
+  const no = {
+    insert	: f => false,
+    update	: f => false,
+    remove	: f => false,
+  };
 
-	  collection.allow( Meteor.isDevelopment ? yes : no );
-	  collection.deny( Meteor.isDevelopment ? no : yes );
+  collection.allow( Meteor.isDevelopment ? yes : no );
+  collection.deny( Meteor.isDevelopment ? no : yes );
 
-	  return ( collection );
-	};
-}
+  return ( collection );
+};
 
 if ( Meteor.isClient ) {
 	global.GEBID = document.getElementById;
